daa25f4a3d3aa8301001e5603c72ac38dd8df704
Noopur <noopurgpt25@gmail.com>
2018-03-04 10:13:35 -0600

    added few more Constants Scott, Noopur

--- Recipe/constants.py
+++ Recipe/constants.py
@@ -6,10 +6,10 @@ vegitarianProteins = ["tofu", "soy", "mixed vegitables", "portabelo mushrooms"]
 meatToVegDict={"chicken":"tofu","steak":"portabelo mushrooms", "chorizo":"tofu", "hot dog": "soy dog", "hamburger":"Veggie Burger", 
 			   "turkey":"soy", "veal":"tofu", "chicken thighs":"tofu","beef":"soy", "chicken broth":"vegitable broth", "chicken stock":"vegitable stock",
 			   "pork":"portabelo mushrooms","salmon":"tofu","veal":"portabelo mushrooms","meat":"non-meat","chicken wings":"tofu", "beef stock": "vegitable stock",
-			   "beef broth":"vegitable broth"}
+			   "beef broth":"vegitable broth", }
 
 measurements=["pound", "pounds", "cup", "cups", "ounce", "ounces", "teaspoon", "teaspoons", "tablespoons","tablespoon" ,"quart", "quarts", "pinch", "taste", "cans"]
-preperation=["ground","diced","chopped","minced","torn", "divided", "pitted", "beaten", "zested"]
+preperation=["ground","diced","chopped","minced","torn", "divided", "pitted", "beaten", "zested", "garnish"]
 
 unHealthItems=["salt","sugar","butter","oil", "lard"]
 
@@ -21,3 +21,8 @@ mexicanIngredients = {"ground cumin":"1 tablespoon","chipotle peppers":"2", "dri
 nonMexicanIngredinet = ["soy sauce", "olives", "ketchup"]
 
 soups=["chicken broth", "chickn stock", "beef broth", "beef stock", "vegitable stock", "vegitable broth"]
+
+tools=["pot", "tablespoons", "frying pan", "skillet", "lid", "skimmer", "oven", "bowl", "knife", "fork", "butter knife","grinder","griller", "grater", "oven mitts", "oven gloves", "plates"]
+
+cookingMethods = ["marinate", "Marinate", "bake", "Bake", "fry", "Fry", "boil", "Boil", "roast", "Roast", "grill" , "Grill", "heat", "Heat", "saute", "Saute", "stir frying",
+				  "deep fry", "shallow fry", "stir fry", "pan fry", "steam","whip", "stir", "drain", "soak"]
